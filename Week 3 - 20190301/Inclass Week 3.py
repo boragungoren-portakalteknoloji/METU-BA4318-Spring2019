7fd1a849b499250e11d396a15f98d1f533f6068c
boragungoren-portakalteknoloji <31903165+boragungoren-portakalteknoloji@users.noreply.github.com>
2019-03-01 13:49:16 +0300

    Committed solution

--- Week 3 - 20190301/Inclass Week 3.py
+++ Week 3 - 20190301/Inclass Week 3.py
@@ -2,3 +2,10 @@
 # Develop a program to get three integer inputs from user,
 # then calculate their sum, and average, and finally
 # print the sum and average
+
+num1 = int ( input("Please enter first integer number") )
+num2 = int ( input("Please enter second integer number") )
+num3 = int ( input("Please enter third integer number") )
+sum = num1 + num2 + num3 
+avg = sum / 3
+print("Sum is ", sum, " and Avg is ", avg)
